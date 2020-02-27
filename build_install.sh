9735a42698e55a4f1ff41ef123019f3820f4602f
msbense <marksbense@gmail.com>
2020-02-27 10:16:59 -0600

    add --config=numa to build script

--- build_install.sh
+++ build_install.sh
@@ -1,5 +1,5 @@
 set -e
-bazel --output_user_root=/dev/shm/tfproj/.cache build //tensorflow/tools/pip_package:build_pip_package --verbose_failures
+bazel --output_user_root=/dev/shm/tfproj/.cache build --config=numa //tensorflow/tools/pip_package:build_pip_package --verbose_failures
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 pip uninstall tensorflow -y
 pip install /tmp/tensorflow_pkg/tensorflow-2.1.0-cp35-cp35m-linux_x86_64.whl
