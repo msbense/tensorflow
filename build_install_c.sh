c522f317032d8f72471ab0da94fd098cd45f07e3
msbense <msbense@node0.msbense-qv71887.hyflowtm-pg0.wisc.cloudlab.us>
2020-06-12 13:39:16 -0500

    remove unused build scripts

--- build_install_c.sh
+++
@@ -1,3 +0,0 @@
-set -e
-bazel --output_base=/mydata/bazel/output build --config opt --config=numa //tensorflow/tools/lib_package:libtensorflow
-sudo tar -C /usr/local -xzf ./bazel-bin/tensorflow/tools/lib_package/libtensorflow.tar.gz
